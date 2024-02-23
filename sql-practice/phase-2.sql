4b70f0a54750e31688666de8ee7bc859274c2ae2
TheSaviourEking <saviour.eking@outlook.com>
2024-02-23 04:32:12 +0100

    chore: implement requirements

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,21 @@
 -- Your code here
+DROP TABLE IF EXISTS customers;
+
+DROP TABLE IF EXISTS coffee_orders;
+
+CREATE TABLE
+    customers (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(40) NOT NULL,
+        phone INTEGER UNIQUE,
+        email VARCHAR(255) UNIQUE,
+        points DEFAULT 5 NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    );
+
+CREATE TABLE
+    coffee_orders (
+        id INTEGER PRIMARY KEY,
+        is_redeemed INTEGER DEFAULT 0,
+        ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    )

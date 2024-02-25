bd650a6de694c8c640044bdb549faf438d7cad82
TheSaviourEking <saviour.eking@outlook.com>
2024-02-26 00:52:30 +0100

    refactor: add action explanations as comments

--- sql-practice/phase-4.sql
+++ sql-practice/phase-4.sql
@@ -5,15 +5,14 @@ DROP TABLE IF EXISTS reviews;
 
 DROP TABLE IF EXISTS parties;
 
--- step 1: Database Schema
--- --- Done
---- step 2: create database
--- -- Done
+.print -- Step 1: Database Schema
+.print --- Done
+.print --- Step 2: Create Database
 CREATE TABLE
   employees (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
+    first_name VARCHAR(20) UNIQUE,
+    last_name VARCHAR(20) UNIQUE,
     department VARCHAR(20),
     role VARCHAR(20),
     relationship_with VARCHAR(20) DEFAULT NULL
@@ -21,14 +20,20 @@ CREATE TABLE
 
 CREATE TABLE
   reviews (
-    employee_id INTEGER,
-    score integer NOT NULL --CHECK (score BETWEEN 1 AND 10)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    employee_id INTEGER REFERENCES employees (id), -- foreign key
+    score integer NOT NULL CHECK (score BETWEEN 1 AND 10)
   );
 
 CREATE TABLE
-  parties (budget DECIMAL(12, 3) NOT NULL, onsite NUMERIC);
+  parties (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    budget DECIMAL(12, 3) NOT NULL,
+    onsite NUMERIC
+  );
 
--- Step 3: Read and Manipulate the Database
+.print -- Step 3: Read and Manipulate the Database
+.print -- Events 3 - 7
 INSERT INTO
   employees (first_name, last_name, department, role)
 VALUES
@@ -60,7 +65,7 @@ VALUES
   ),
   ('Roy', 'Anderson', 'Warehouse', 'Warehouse Staff');
 
--- 8:
+.print -- Events 8. "Roy Anderson" and "Pam Beesly" are in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = (
@@ -87,21 +92,21 @@ SET
 WHERE
   first_name = 'Pam';
 
--- 9
+.print -- Event 9. "Ryan Howard" is hired in the "Reception" department as a "Temp" role.
 INSERT INTO
   employees (first_name, last_name, department, role)
 VALUES
   ('Ryan', 'Howard', 'Reception', 'Temp');
 
--- 10
+.print -- Event 10. An onsite office party is scheduled with a budget of $100.00.
 INSERT INTO
-  parties
+  parties (budget, onsite)
 VALUES
   (100.00, 1);
 
--- 11
+.print -- Event 11. "Dwight Schrute" gets a performance review with a score of 3.3.
 INSERT INTO
-  reviews
+  reviews (employee_id, score)
 SELECT
   id,
   3.3
@@ -111,16 +116,7 @@ WHERE
   first_name = 'Dwight'
   AND last_name = 'Schrute';
 
--- 12
--- INSERT INTO
---   reviews
--- SELECT
---   id,
---   4.2
--- FROM
---   employees
--- WHERE
---   first_name = 'Jim' and last_name = 'Halpert';
+.print -- Event 12. "Jim Halpert" gets a performance review with a score of 4.2.
 INSERT INTO
   reviews (employee_id, score)
 VALUES
@@ -136,7 +132,7 @@ VALUES
     4.2
   );
 
--- 13
+.print -- Event 13. "Dwight Schrute"'s past performance review needs to be changed to a score of 9.0.
 UPDATE reviews
 SET
   score = 9.0
@@ -151,7 +147,7 @@ WHERE
       AND last_name = 'Schrute'
   );
 
--- 14
+.print -- Event 14. "Jim Halpert"'s past performance review needs to be changed to a score of 9.3.
 UPDATE reviews
 SET
   score = 9.3
@@ -166,7 +162,7 @@ WHERE
       AND last_name = 'Halpert'
   );
 
--- 15
+.print -- Event 15. "Jim Halpert" is promoted to the role of "Assistant Regional Manager".
 UPDATE employees
 SET
   role = 'Assistant Regional Manager'
@@ -174,7 +170,7 @@ WHERE
   first_name = 'Jim'
   AND last_name = 'Halpert';
 
--- 16
+.print -- Event 16. "Ryan Howard" is promoted to the "Sales" department as the role of "Sales Representative".
 UPDATE employees
 SET
   department = 'Sales',
@@ -183,13 +179,13 @@ WHERE
   first_name = 'Ryan'
   AND last_name = 'Howard';
 
--- 17
+.print -- Event 17. An onsite office party is scheduled with a budget of $200.00.
 INSERT INTO
-  parties
+  parties (budget, onsite)
 VALUES
   (200, 1);
 
--- 18
+.print -- Event 18. "Angela Martin" and "Dwight Schrute" are in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = (
@@ -220,9 +216,9 @@ WHERE
   first_name = 'Dwight'
   AND last_name = 'Schrute';
 
--- 19
+-- Event 19. "Angela Martin" gets a performance review score of 6.2.
 INSERT INTO
-  reviews
+  reviews (employee_id, score)
 SELECT
   id,
   6.2
@@ -232,7 +228,7 @@ WHERE
   first_name = 'Angela'
   AND last_name = 'Martin';
 
--- 20
+-- Event 20. "Ryan Howard" and "Kelly Kapoor" are in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = (
@@ -263,13 +259,13 @@ WHERE
   first_name = 'Ryan'
   AND last_name = 'Howard';
 
--- 21
+-- Event 21. An onsite office party is scheduled with a budget of $50.00.
 INSERT INTO
-  parties
+  parties (budget, onsite)
 VALUES
   (50, 1);
 
---22
+--Event 22. "Jim Halpert" moves to another office branch (make sure to remove his relationships and
 -- Jim moves to another office, i'll just set his department and all else to null;
 UPDATE employees
 SET
@@ -280,7 +276,7 @@ WHERE
   first_name = 'Jim'
   AND last_name = 'Halpert';
 
--- 23
+-- Event 23. "Roy Anderson" and "Pam Beesly" are NO LONGER in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = NULL
@@ -295,9 +291,9 @@ WHERE
   first_name = 'Pam'
   AND last_name = 'Beesly';
 
--- 24
+-- Event 24. "Pam Beesly" gets a performance review score of 7.6.
 INSERT INTO
-  reviews
+  reviews (employee_id, score)
 SELECT
   id,
   7.6
@@ -307,9 +303,9 @@ WHERE
   first_name = 'Pam'
   AND last_name = 'Beesly';
 
--- 25
+-- Event 25. "Dwight Schrute" gets another performance review score of 8.7.
 INSERT INTO
-  reviews
+  reviews (employee_id, score)
 SELECT
   id,
   8.7
@@ -318,7 +314,7 @@ FROM
 WHERE
   first_name = 'Dwight';
 
--- 26
+-- Event 26. "Ryan Howard" quits the office (make sure to remove his relationships and performance reviews if he has any).
 -- UPDATE employees
 -- set department = NULL, role = NULL, relationship_with = NULL
 -- where first_name = 'Ryan';
@@ -326,7 +322,7 @@ DELETE FROM employees
 WHERE
   first_name = 'Ryan';
 
--- 27
+.print -- Event 27. "Jim Halpert" moves back to this office branch's "Sales" department in the role of "Sales Representative"
 UPDATE employees
 SET
   department = 'Sales',
@@ -335,7 +331,7 @@ WHERE
   first_name = 'Jim'
   AND last_name = 'Halpert';
 
--- 28
+-- Event 28. "Karen Filippelli" moves from a different office into this office's "Sales" department in the role of "Sales Representative"
 INSERT INTO
   employees (first_name, last_name, department, role)
 VALUES
@@ -346,7 +342,7 @@ VALUES
     'Sales Representative'
   );
 
--- 29
+-- Event 29. "Karen Filippelli" and "Jim Halpert" are in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = (
@@ -375,14 +371,27 @@ SET
 WHERE
   first_name = 'Jim';
 
--- 30
+-- Event 30. An onsite office party is scheduled with a budget of $120.00.
 INSERT INTO
-  parties
+  parties (budget, onsite)
 VALUES
   (120, 1);
 
--- 31
--- 32
+-- Event 31. The onsite office party scheduled right before this is canceled, and an offsite office party is scheduled instead with a budget of $300.00
+-- .print "--- CHHHHHHH-----------------"
+-- .shell echo "--- CHHHHHHH-----------------";
+DELETE FROM parties
+ORDER BY
+  id DESC
+LIMIT
+  1;
+
+INSERT INTO
+  parties (budget, onsite)
+VALUES
+  (300, 0);
+
+-- Event 32. "Karen Filippelli" and "Jim Halpert" are NO LONGER in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = NULL
@@ -395,7 +404,7 @@ SET
 WHERE
   first_name = 'Jim';
 
--- 33
+-- Event 33. "Pam Beesly" and "Jim Halpert" are in a romantic relationship.
 UPDATE employees
 SET
   relationship_with = (
@@ -422,7 +431,7 @@ SET
 WHERE
   first_name = 'Pam';
 
--- Debugging
+.print ---------------------------------------------- DEBUGGING -----------------------------------------------------------
 SELECT
   *
 FROM
